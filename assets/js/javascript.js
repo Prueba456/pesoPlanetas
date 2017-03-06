343022f6dd0bd3cda3d7b444bf13e754dd9a86ba
betsiana <betsi18_12@hotmail.com>
2017-03-05 23:45:22 -0500

    peso en mas planetas

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -3,6 +3,8 @@ var planeta = parseInt(prompt("Elige tu planeta \n1. marte 2.jupiter"));
 var gravedadTierra = 9.8;
 var gravedadMarte = 3.7;
 var gravedadJupiter = 24.8;
+var gravedadSaturno = 9.05;
+var gravedadUrano = 7.77;
 var resultado = document.getElementById('mostrar');
 var pesoFinal;
 if(planeta ==1){
@@ -15,7 +17,14 @@ if(planeta ==2){
   pesoFinal = parseInt(pesoFinal);
   planeta = "jupiter";
 }
-
-
-
+if(planeta ==3){
+  pesoFinal = peso*gravedadSaturno / gravedadTierra;
+  pesoFinal = parseInt(pesoFinal);
+  planeta = "saturno";
+}
+if(planeta ==4){
+  pesoFinal = peso*gravedadUrano / gravedadTierra;
+  pesoFinal = parseInt(pesoFinal);
+  planeta = "urano";
+}
 resultado.innerHTML = "tu peso en "+planeta+"  es: "+pesoFinal;
